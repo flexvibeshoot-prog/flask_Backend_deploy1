a5b01e5edcbac4d9078fab16ed2c5eceeea8679b
Flexandvibeshoot <flexvibeshoot@gmail.com>
2026-01-20 02:19:07 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+        use_reloader=False)
